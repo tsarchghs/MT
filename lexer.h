cd94ca5c87a5890e8697a5f932743219b04f0caa
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 21:51:10 +0100

    Moved struct token to lexer.h

--- lexer.h
+++ lexer.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "helpers.h"
 
+struct token
+{
+	char *type;
+	char *value;
+	struct token *next;
+};
+
+
 void lex(char *sCode,struct token *rootToken);
 int numberLaH(char string[],int sI,size_t sz); // number lookahead
 int stringLaH(char string[],int sI,size_t sz,int type); // string lookahead
