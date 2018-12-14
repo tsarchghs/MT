41249945336a2a0037229db4988661a3d1460d5c
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 16:30:58 +0100

    Removed sliceString prototype

--- lexer.h
+++ lexer.h
@@ -11,4 +11,3 @@ struct token
 void lex(char *sCode,struct token *rootToken);
 int numberLaH(char string[],int sI,size_t sz); // number lookahead
 int stringLaH(char string[],int sI,size_t sz,int type); // string lookahead
-int sliceString(char string[],int sI,int eI,size_t sz,char *location);
