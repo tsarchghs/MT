8583f3b80bc8b800feacad74c60e61853e6d156b
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 16:32:39 +0100

    input allocates space for 255chars,added show/all show/rootToken

--- compiler.c
+++ compiler.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lexer.h"
+#include <string.h>
 
 int main(void){
-	struct token rootToken = {.type=NULL,.value=NULL,.next=NULL};
-	char lexelize;
+	struct token rootToken = {.type="root",.value=NULL,.next=NULL};
+	char *input = malloc(sizeof(char) * 255);
 	while (1){
-		printf(">>> ");
-		scanf("%[^\n]%*c",&lexelize);
-		lex(&lexelize,&rootToken);
+		printf(">>> "); 
+		scanf("%[^\n]%*c",input);
+		if (strcmp(input,"show/all") == 0){
+			struct token *cToken = &rootToken;
+			int x = 0;
+			while (cToken->next != NULL){
+				printf("<%d> Token: [%s] [%s] [%p]\n",x,cToken->type,cToken->value,cToken->next);
+				cToken = cToken->next;
+				x++;	
+			}
+			printf(">>> ");
+		} else if (strcmp(input,"show/rootToken") == 0){
+				printf("<%d> Root token : [%s] [%s] [%p]\n",0,rootToken.type,rootToken.value,rootToken.next);			
+		}
+		 else {
+			lex(input,&rootToken);
+		}
 	}
 }
