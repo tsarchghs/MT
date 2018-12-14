d9d8b93e0199c0c959bdb3282492e6260204369a
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 21:51:27 +0100

    Removed structs declarations

--- helpers.h
+++ helpers.h
@@ -1,19 +1,3 @@
 #include <stdlib.h>
 
-struct token
-{
-	char *type;
-	char *value;
-	struct token *next;
-};
-
-struct tree
-{
-	char *type;
-	struct token *token;
-	struct tree *next;
-};
-
-
-
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
