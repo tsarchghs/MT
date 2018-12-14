dbc5db1bbeb155c081a1afadf9dd153333311fca
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 21:01:03 +0100

    Added option to pass source file ./compiler hello.md

--- compiler.c
+++ compiler.c
@@ -3,27 +3,52 @@
 #include "lexer.h"
 #include <string.h>
 
-int main(void){
+int main(int argc,char *argv[]){
 	struct token rootToken = {.type="root",.value=NULL,.next=NULL};
-	char *input = malloc(sizeof(char) * 255);
-	while (1){
-		printf(">>> "); 
-		scanf("%[^\n]%*c",input);
-		if (strcmp(input,"show/all") == 0){
-			struct token *cToken = &rootToken;
-			int x = 0;
-			while (cToken->next != NULL){
-				printf("<%d> Token: [%s] [%s] [%p]\n",x,cToken->type,cToken->value,cToken->next);
-				cToken = cToken->next;
-				x++;	
-			}
-			printf("<%d> Last token: [%s] [%s] [%p]\n",x,cToken->type,cToken->value,cToken->next);
-			printf(">>> ");
-		} else if (strcmp(input,"show/rootToken") == 0){
-				printf("<%d> Root token : [%s] [%s] [%p]\n",0,rootToken.type,rootToken.value,rootToken.next);			
+	if (argc > 1){
+		char * buffer = 0;
+		long length;
+		FILE * f = fopen (argv[1], "rb");
+
+		if (f) {
+		  fseek (f, 0, SEEK_END);
+		  length = ftell (f);
+		  fseek (f, 0, SEEK_SET);
+		  buffer = malloc (length);
+		  if (buffer){
+		    fread (buffer, 1, length, f);
+		  }
+		  fclose (f);
+		} else {
+			printf("File %s not found.\n",argv[1]);
+			return 1;
 		}
-		 else {
-			lex(input,&rootToken);
+
+		if (buffer){
+			//printf("%s\n",buffer);
+			lex(buffer,&rootToken);
+		}
+	} else {
+		char *input = malloc(sizeof(char) * 255);
+		while (1){
+			printf(">>> "); 
+			scanf("%[^\n]%*c",input);
+			if (strcmp(input,"show/all") == 0){
+				struct token *cToken = &rootToken;
+				int x = 0;
+				while (cToken->next != NULL){
+					printf("<%d> Token: [%s] [%s] [%p]\n",x,cToken->type,cToken->value,cToken->next);
+					cToken = cToken->next;
+					x++;	
+				}
+				printf("<%d> Last token: [%s] [%s] [%p]\n",x,cToken->type,cToken->value,cToken->next);
+				printf(">>> ");
+			} else if (strcmp(input,"show/rootToken") == 0){
+					printf("<%d> Root token : [%s] [%s] [%p]\n",0,rootToken.type,rootToken.value,rootToken.next);			
+			}
+			 else {
+				lex(input,&rootToken);
+			}
 		}
 	}
 }
