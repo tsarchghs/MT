14e67a4df5693c9bbb7d6443853f73661198fce4
gjergjk71 <gjergjk71@gmail.com>
2018-12-14 20:22:41 +0100

    Created struct tree

--- helpers.h
+++ helpers.h
@@ -7,5 +7,13 @@ struct token
 	struct token *next;
 };
 
+struct tree
+{
+	char *type;
+	struct token *token;
+	struct tree *next;
+};
+
+
 
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
