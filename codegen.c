660ab13fb222bd423accbb13881e4a46284ddc82
gjergjk71 <gjergjk71@gmail.com>
2018-12-17 10:46:32 +0100

    Generates code for if statements

--- codegen.c
+++ codegen.c
@@ -1,9 +1,10 @@
 #include <string.h>
 #include "codegen.h"
+#include "helpers.h"
 
 void generate(struct token *rootToken,char *code){
 	int sz = 0;
-	int i;
+	int inConditional = 0;
 	//strcpy(code,START);
 	//sz += sizeof(START) / sizeof(START[0]);
 	do {
@@ -13,35 +14,34 @@ void generate(struct token *rootToken,char *code){
 				sz += 4;
 			}
 			strcpy(code + sz,rootToken->value);
-			i = 0;
-			for (int x=0;1;i++){
-				if (rootToken->value[i] == '\0'){
-					break;
-				}
-			}
-			sz += i;
+			sz += count(rootToken->value);
 			code[sz] = ' ';
 			sz++;
+			if (rootToken->next->type[0] == ':' && inConditional){
+				code[sz] = ')';
+				sz++;
+				inConditional = 0;
+			}
 		} else if (strcmp(rootToken->type,"assignment") == 0 ||
 				   strcmp(rootToken->type,"operator") == 0 || 
 				   strcmp(rootToken->type,";") == 0 ||
 				   strcmp(rootToken->type,"number") == 0){
 			strcpy(code + sz,rootToken->value);
-			i = 0;
-			for (int x=0;1;i++){
-				if (rootToken->value[i] == '\0'){
-					break;
-				}
-			}
-			sz += i;
+			sz += count(rootToken->value);
 			code[sz] = ' ';
 			sz++;
 		} else if (strcmp(rootToken->type,"end") == 0){
 			code[sz] = '}';
 			sz++;
-		} else if (strcmp(rootToken->type,":")){
+		} else if (rootToken->type[0] == ':'){
 			code[sz] = '{';
 			sz++;
+		} else if (strcmp(rootToken->type,"conditional") == 0){
+			strcpy(code + sz,rootToken->value);
+			sz += count(rootToken->value);
+			code[sz] = '(';
+			sz++;
+			inConditional = 1;
 		}
 		rootToken = rootToken->next;
 	} while (rootToken != NULL); // make sure to add support for last token
