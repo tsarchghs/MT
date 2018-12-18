0ce3b047d8b47a194169520c05953aa5b559ed9b
gjergjk71 <gjergjk71@gmail.com>
2018-12-18 21:08:35 +0100

    Added inApostrophe arg

--- lexer.h
+++ lexer.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "helpers.h"
+#include <stdbool.h>
 
 struct token
 {
@@ -11,4 +12,4 @@ struct token
 
 void lex(char *sCode,struct token *rootToken);
 int numberLaH(char string[],int sI,size_t sz); // number lookahead
-int stringLaH(char string[],int sI,size_t sz,int type); // string lookahead
+int stringLaH(char string[],int sI,size_t sz,int type,bool inApostrophe); // string lookahead
