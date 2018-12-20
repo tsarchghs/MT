61429dfbbf03ca5277744e986d4734b68ac06d2b
gjergjk71 <gjergjk71@gmail.com>
2018-12-20 17:27:24 +0100

    Prototypes of const variables

--- helpers.h
+++ helpers.h
@@ -1,4 +1,18 @@
 #include <stdlib.h>
 
+const int ROOT;
+const int DECLARATION ;
+const int ASSIGNMENT ;
+const int STRING ;
+const int NUMBER ;
+const int SYMBOL ;
+const int OPERATOR ;
+const int CURLYBRACE ;
+const int CONDITIONAL ;
+const int END ;
+const int IF;
+const int COLON;
+const int COMPARISION;
+
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
 int count(char string[]);
