c01f1b81a8e983a670c1cb671c23bd31cb1b84ab
gjergjk71 <gjergjk71@gmail.com>
2018-12-21 16:40:49 +0100

    Added support for assigning variable to variable

--- codegen.c
+++ codegen.c
@@ -5,40 +5,95 @@
 void generate(struct token *rootToken,char *code){
 	int sz = 0;
 	int inConditional = 0;
+	int isInt = 0;
 	int isString = 0;
+	int declaring = 0;
+	int afterAssignment = 0;
+	struct symbol root_symbol = {.next=NULL,.value=NULL,.symbol_token=NULL,.dataType=0};
 	//strcpy(code,START);
 	//sz += sizeof(START) / sizeof(START[0]);
+	struct symbol *cSymbol = &root_symbol; // current symbol
+	struct symbol *symbolLoc = NULL;
+	int convert = 0; // if convert=1 when symbol token found it will write it's value instead of the variable name
 	do {
+		struct symbol *nxtSymbol = malloc(sizeof(struct symbol));
 		if (rootToken->type == DECLARATION){
-			if (rootToken->next->next->type == ASSIGNMENT){
-				if (rootToken->next->next->next->type ==STRING){
-					strcpy(code+sz,"char ");
+			declaring = 1;
+			symbolLoc = malloc(sizeof(struct symbol));
+			int decl_type = dtLaH(rootToken,&root_symbol,symbolLoc);
+			if (decl_type == 1 || decl_type == 2){
+				convert = 1;
+				nxtSymbol->symbol_token = rootToken->next->next->next;
+				if (symbolLoc->dataType == NUMBER){
+					isInt = 1;
+					nxtSymbol->dataType = NUMBER;
+				} else if (symbolLoc->dataType == STRING) {
 					isString = 1;
-					sz += 5;
-				} else if (rootToken->next->next->next->type == NUMBER){
-					strcpy(code+sz,"int ");
-					sz += 4;
+					nxtSymbol->dataType = STRING;
+				}
+
+				cSymbol->next = nxtSymbol;
+				cSymbol = cSymbol->next;
+			} else {
+				if (decl_type == -1){
+					printf("Parse error\n");
+				} else {
+					struct token *assignmentT = rootToken->next->next;
+					nxtSymbol->symbol_token = rootToken->next;
+					nxtSymbol->dataType = assignmentT->next->type;
+					nxtSymbol->value = assignmentT->next->value;
+					cSymbol->next = nxtSymbol;
+					cSymbol = cSymbol->next;
+					//printf("cSymbol -> [%s] [%d] [%s]\n",cSymbol->symbol_token->value,cSymbol->dataType,cSymbol->value);
+					if (decl_type == NUMBER){
+						isInt = 1;
+					} else {
+						isString = 1;
+					}
 				}
 			}
+			if (isInt){
+				strcpy(code+sz,"int ");
+				sz += 4;
+				isInt = 0;
+			} else if (isString) {
+				strcpy(code+sz,"char ");
+				isString = 1;
+				sz += 5;
+			}
 		} else if (rootToken->type == SYMBOL){
-			strcpy(code + sz,rootToken->value);
-			sz += count(rootToken->value);
-			code[sz] = ' ';
-			sz++;
+			if (declaring && afterAssignment && convert && 
+					(strcmp(symbolLoc->symbol_token->value,rootToken->value)) == 0){
+
+				strcpy(code + sz,symbolLoc->value);
+				sz += count(symbolLoc->value);
+				code[sz] = ' ';
+				sz++;
+				convert = 0;
+			} else {
+				strcpy(code + sz,rootToken->value);
+				sz += count(rootToken->value);
+				code[sz] = ' ';
+				sz++;
+			}
+			if (isString){
+				strcpy(code+sz,"[] ");
+				sz += 3;
+				isString = 0;
+			}
 			if (rootToken->next != NULL && rootToken->next->type == COLON && inConditional){
 				code[sz] = ')';
 				sz++;
 				inConditional = 0;
-			} else if (isString){
-				strcpy(code+sz,"[] ");
-				sz += 3;
-				isString = 0;
 			}
 		} else if (rootToken->type == ASSIGNMENT ||
 				   rootToken->type == OPERATOR ||
 				   rootToken->type == STRING || 
 				   rootToken->type == CURLYBRACE ||
 				   rootToken->type == NUMBER){
+			if (rootToken->type == ASSIGNMENT){
+				afterAssignment = 1;
+			}
 			strcpy(code + sz,rootToken->value);
 			sz += count(rootToken->value);
 			code[sz] = ' ';
@@ -60,11 +115,3 @@ void generate(struct token *rootToken,char *code){
 	} while (rootToken != NULL); // make sure to add support for last token
 	code[sz] = '\0';
 }
-
-/*int dLookahead(struct token *token){ // declaration lookahead
-	int type;
-
-	if (token->next->type == "assignment"){
-		if (token->next->next->type )
-	}
-}*/
