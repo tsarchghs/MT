1426cfab7cc123d7fe132abf311c999565b7cbe9
gjergjk71 <gjergjk71@gmail.com>
2018-12-29 18:26:22 +0100

    Defined INTEGER/FLOAT_ const int

--- helpers.h
+++ helpers.h
@@ -4,7 +4,7 @@ const int ROOT;
 const int DECLARATION ;
 const int ASSIGNMENT ;
 const int STRING ;
-const int NUMBER ;
+const int INTEGER ;
 const int SYMBOL ;
 const int OPERATOR ;
 const int SEMICOLON ;
@@ -13,6 +13,7 @@ const int END ;
 const int COLON;
 const int COMMENT;
 const int PARENTHESIS;
+const int FLOAT_;
 
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
 int count(char string[]);
