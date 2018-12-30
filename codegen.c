179712483cd09ba7f1783c5daa68ca60ef8db3a7
gjergjk71 <gjergjk71@gmail.com>
2018-12-30 17:31:18 +0100

    generate function handles decl_type==4 now, +bugfixes

--- codegen.c
+++ codegen.c
@@ -2,6 +2,8 @@
 #include "codegen.h"
 #include "helpers.h"
 
+// I sincirely apologise to my future self for turning this function into spaghetti code on 12/30/2018
+
 void generate(struct token *rootToken,char *code){
 	int sz = 0;
 	int inConditional = 0;
@@ -17,13 +19,16 @@ void generate(struct token *rootToken,char *code){
 	struct symbol *cSymbol = &root_symbol; // current symbol
 	struct symbol *symbolLoc = NULL;
 	int convert = 0; // if convert=1 when symbol token found it will write it's value instead of the variable name
+	int expr_to_var = 0;
 	do {
 		struct symbol *nxtSymbol = malloc(sizeof(struct symbol));
 		if (rootToken->type == DECLARATION){
 			declaring = 1;
 			symbolLoc = malloc(sizeof(struct symbol));
 			int decl_type = dtLaH(rootToken,&root_symbol,symbolLoc);
-			if (decl_type == 1 || decl_type == 2 || decl_type == 3){
+			if (decl_type == 4){
+				expr_to_var = 1;
+			} else if (decl_type == 1 || decl_type == 2 || decl_type == 3){
 				convert = 1;
 				struct token *assignmentT = rootToken->next->next;
 				nxtSymbol->value = assignmentT->next->value;
@@ -52,32 +57,96 @@ void generate(struct token *rootToken,char *code){
 
 			}
 		} else if (rootToken->type == SYMBOL){
-			if (declaring && afterAssignment && convert && 
-					(symbolLoc != NULL && strcmp(symbolLoc->symbol_token->value,rootToken->value)) == 0){
-				char repr[] = " {.type=%d,.%s=%s.%s}";
+			if (declaring && afterAssignment && expr_to_var){
+				char repr[] = " {.type=%d,.%s=";
+				int isF = 0;
+				struct token *original = rootToken;
+				struct symbol *tmpSymbol = malloc(sizeof(struct symbol));
+				while (rootToken != NULL && rootToken->type != SEMICOLON){
+					if (rootToken->type == FLOAT_){
+						isF = 1;
+						break;
+					} else if (rootToken->type == SYMBOL){
+						int found = findSymbol(&root_symbol,rootToken->value,tmpSymbol);
+						if (found){
+							if (tmpSymbol->dataType == FLOAT_){
+								isF = 1;
+								break;
+							}
+						}
+					}
+					rootToken = rootToken->next;
+				}
+				rootToken = original;
 				char repr2[500];
-				//printf("%d - %s - %d - %s \n",symbolLoc->dataType,symbolLoc->value,symbolLoc->symbol_token->type,symbolLoc->symbol_token->value);
-				if (symbolLoc->dataType == INTEGER){
-					sprintf(repr2,repr,
-						symbolLoc->dataType,
-						"integer",
-						symbolLoc->symbol_token->value,
-						"integer");
-				} else 	if (symbolLoc->dataType == FLOAT_){
-					sprintf(repr2,repr,
-						symbolLoc->dataType,
-						"float_",
-						symbolLoc->symbol_token->value,
-						"float_");
+				if (isF){
+					sprintf(repr2,repr,FLOAT_,"float_");
+				} else {
+					sprintf(repr2,repr,INTEGER,"integer");					
 				}
 				strcpy(code + sz,repr2);
 				sz += count(repr2);
-				convert = 0;
-				printf("11123\n");
-				afterAssignment = 0;
+				//struct symbol *tmpSymbol = malloc(sizeof(struct symbol));
+				while (rootToken != NULL){
+					if (rootToken->type == SYMBOL){
+						strcpy(code + sz,rootToken->value);
+						sz += count(rootToken->value);
+						int found = findSymbol(&root_symbol,rootToken->value,tmpSymbol);
+						if (found){
+							if (tmpSymbol->dataType == INTEGER){
+								strcpy(code + sz,".integer ");
+								sz += 9;
+							} else if (tmpSymbol->dataType == FLOAT_){
+								strcpy(code + sz,".float_ ");
+								sz += 8;							
+							}
+						} else {
+							printf("Var not found!\n");
+						}
+					} else {
+						strcpy(code + sz,rootToken->value);
+						sz += count(rootToken->value);
+					}
+					rootToken = rootToken->next;
+					if (rootToken->type == SEMICOLON){
+						break;
+					}
+				}
+				strcpy(code+sz,"}");
+				sz++;
 				declaring = 0;
+				expr_to_var = 0;
+				afterAssignment = 0;
+			} 
+			if (declaring && afterAssignment && convert && 
+					(symbolLoc != NULL && strcmp(symbolLoc->symbol_token->value,rootToken->value)) == 0){
+				if (rootToken->next->type == SEMICOLON){
+					char repr[] = " {.type=%d,.%s=%s.%s}";
+					char repr2[500];
+					//printf("%d - %s - %d - %s \n",symbolLoc->dataType,symbolLoc->value,symbolLoc->symbol_token->type,symbolLoc->symbol_token->value);
+					if (symbolLoc->dataType == INTEGER){
+						sprintf(repr2,repr,
+							symbolLoc->dataType,
+							"integer",
+							symbolLoc->symbol_token->value,
+							"integer");
+					} else 	if (symbolLoc->dataType == FLOAT_){
+						sprintf(repr2,repr,
+							symbolLoc->dataType,
+							"float_",
+							symbolLoc->symbol_token->value,
+							"float_");
+					}
+					strcpy(code + sz,repr2);
+					sz += count(repr2);
+					convert = 0;
+					printf("11123\n");
+					afterAssignment = 0;
+					declaring = 0;
+				}
 			} else {
-				if (declaring && rootToken->next->next->type == SYMBOL){ //
+				if (declaring && rootToken->next != NULL && rootToken->next->next != NULL 
+						&& rootToken->next->next->type == SYMBOL){ //
 					strcpy(code + sz," struct mt_object ");
 					sz += 18;
 					strcpy(code + sz,rootToken->value);
@@ -98,7 +167,12 @@ void generate(struct token *rootToken,char *code){
 						}
 						printf("'--> %d\n",tmpSymbol->dataType);
 					} else {
-						printf("ERROR: %s is undefined\n",rootToken->value);
+						if (rootToken->value[0] == ';'){ // ugly I know :'(
+							strcpy(code + sz,";");
+							sz++;
+						} else {
+							printf("ERROR: %s is undefined\n",rootToken->value);
+						}
 					}
 				} else {
 					strcpy(code + sz,rootToken->value);
@@ -112,6 +186,10 @@ void generate(struct token *rootToken,char *code){
 			}
 			code[sz] = ' ';
 			sz++;
+		} else if (rootToken != NULL && rootToken->next != NULL && rootToken->type == COLON && inConditional){
+				strcpy(code + sz,"){");
+				sz += 2;
+				inConditional = 0;
 		} else if (rootToken->type == ASSIGNMENT ||
 				   rootToken->type == OPERATOR ||
 				   rootToken->type == STRING || 
