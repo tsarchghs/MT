a86f8a9c444d284822f76d7fb15f8e6816afd571
gjergjk71 <gjergjk71@gmail.com>
2018-12-31 12:18:18 +0100

    Updated findSymbol prototype

--- helpers.h
+++ helpers.h
@@ -19,4 +19,4 @@ const int FLOAT_;
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
 int count(char string[]);
 int dtLaH(struct token *token,struct symbol *symbol_token,struct symbol *location); // declaration type look-a-head
-int findSymbol(struct symbol *root_symbol,char *name,struct symbol *location); // find symbol value
+int findSymbol(struct symbol *root_symbol,char *name,struct symbol **location); // find symbol value
