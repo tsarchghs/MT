55a119bc206c3b0cd8ac9197193b366363384fb8
gjergjk71 <gjergjk71@gmail.com>
2019-01-04 16:29:17 +0100

    Added mode arg to dtLaH

--- helpers.h
+++ helpers.h
@@ -18,5 +18,5 @@ const int FLOAT_;
 
 int sliceString(char string[],int sI,int eI,size_t sz,char *location);
 int count(char string[]);
-int dtLaH(struct token *token,struct symbol *symbol_token,struct symbol *location); // declaration type look-a-head
+int dtLaH(struct token *token,struct symbol *symbol_token,struct symbol *location,int mode); // declaration type look-a-head
 int findSymbol(struct symbol *root_symbol,char *name,struct symbol **location); // find symbol value
