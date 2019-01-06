ae60eeeeaaaf1a595531d2df5c3bdd6e7f20cfb8
gjergjk71 <gjergjk71@gmail.com>
2019-01-06 13:11:54 +0100

    Created writeType func prototype

--- helpers.h
+++ helpers.h
@@ -27,3 +27,4 @@ int count(char string[]);
 int dtLaH(struct token *token,struct symbol *symbol_token,struct symbol *location,int mode); // declaration type look-a-head
 int findSymbol(struct symbol *root_symbol,char *name,struct symbol **location); // find symbol value
 int findPSymbol(struct param *root_param,char *name,struct param **location); // find parameter symbol value
+void writeType(char **code,int *sz,int type);
