5c74e56ee290ab17cd2dbf474f2aa95daf3afb28
gjergjk71 <gjergjk71@gmail.com>
2019-01-06 12:40:15 +0100

    Changed struct param childrens

--- codegen.h
+++ codegen.h
@@ -1,9 +1,13 @@
+#ifndef CODEGEN_H
+#define CODEGEN_H
 #include "lexer.h"
 
 struct param {
 	int type;
-	struct symbol *var;
-	struct symbol *next;
+	struct token *token;
+	struct symbol *symbol_ptr;
+	struct param *next;
 };
 
 void generate(struct token *rootToken,char *code);
+#endif
