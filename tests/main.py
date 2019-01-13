1ed268168811f38c49ce749436b4f1a23fcbfaaf
gjergjk71 <gjergjk71@gmail.com>
2019-01-13 17:10:48 +0100

    Added source/test_variables.mt to tests

--- tests/main.py
+++ tests/main.py
@@ -2,6 +2,7 @@ from subprocess import Popen,PIPE,STDOUT
 from os.path import dirname,abspath
 from os import environ
 
+get_output = True
 
 compiler_path = dirname(dirname(abspath(__file__))) + "/compiler"
 
@@ -10,6 +11,11 @@ tests = [
 		"source":"source/test_print.mt",
 		"cli_output":b"Compiled.\n",
 		"output":b'1\n1.500000\nHello, World!\n'
+	},
+	{
+		"source":"source/test_variables.mt",
+		"cli_output":b"Compiled.\n",
+		"output":b'DSASDA\n31.100000\n52\n52\nhelloo\n'
 	}
 ]
 
@@ -19,12 +25,16 @@ class TestCase:
 		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
 				stderr=STDOUT, close_fds=True)
 		output = p.stdout.read()
+		if get_output:
+			print(output)
 		if output != expected_output:
 			print(f"{output} != {expected_output} (compile)");
 	def execute(self,exec_file,expected_output):
 		p = Popen(exec_file, shell=True, stdin=PIPE, stdout=PIPE,
 				stderr=STDOUT, close_fds=True)
 		output = p.stdout.read()
+		if get_output:
+			print(output)
 		if output != expected_output:
 			print(f"{output} != {expected_output} (execute)");
 
